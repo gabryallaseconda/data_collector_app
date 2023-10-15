375d9994fa5d35725b74dc852f442d4961a80e6e
gabryallaseconda <gabriele.gabrielli.96@gmail.com>
2023-10-15 17:36:22 +0200

    paired to chap 5

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from flask import Flask
-from config import DevConfig
-app = Flask(__name__)
-app.config.from_object(DevConfig)
+import os
+from webapp import create_app
 
-@app.route('/')
-def home():
-    return '<h1>Hello World!</h1>'
+env = os.environ.get('WEBAPP_ENV', 'dev')
+app = create_app('config.%sConfig' % env.capitalize())
+
 
 if __name__ == '__main__':
     app.run()
